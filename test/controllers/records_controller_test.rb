dcc8af74f75b96cc7c38d55e7efbfd1181dd5a52
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-06 23:27:25 +0900

    init

---
+++ test/controllers/records_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class RecordsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get records_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get records_edit_url
+    assert_response :success
+  end
+
+end
