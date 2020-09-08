759f9b7fed6a778f661a87296debf3a609d1e17c
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-08 12:56:51 +0900

    genreCRUD実装完了

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,42 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_account!
+
   def index
     @categories = Category.all
   end
 
   def new
     @category = Category.new
+    account = current_account
+    @category.account_id = account.id
   end
 
   def create
-    Category.create(categories_params)
-    redirect_to "/categories"
+    @category = Category.new(categories_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(categories_params)
+      redirect_to categories_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/new'
-  root "records#index"
-  # get 'records/index'
-  # get 'records', to: 'records#index'
-  # get 'records/edit'
+  resources :categories
   resources :records
   devise_for :accounts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
