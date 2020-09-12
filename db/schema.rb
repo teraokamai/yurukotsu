36df54ee246f63770a98a814b3f4143146d87b42
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-12 20:35:23 +0900

    記録編集画面レイアウト修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_123036) do
+ActiveRecord::Schema.define(version: 2020_09_12_090852) do
+
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_123036) do
     t.time "end_at"
     t.string "summary"
     t.text "content"
-    t.time "total"
+    t.integer "total"
     t.boolean "calculation", null: false
     t.integer "account_id"
     t.integer "category_id"
