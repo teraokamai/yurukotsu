85749b61f2800e0ee6956a0062a30b6f1c996317
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-14 18:59:03 +0900

    ActionText実装

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,7 @@
 class Record < ApplicationRecord
   belongs_to :account
   belongs_to :category
+  has_rich_text :content
 
-  validates :do_on, presence: { message: "は必須項目です。" }
+  validates :do_on, :start_at, :end_at, :summary, :total, :calculation, presence: { message: "は必須項目です。" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_090852) do
+ActiveRecord::Schema.define(version: 2020_09_14_084545) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,37 @@ ActiveRecord::Schema.define(version: 2020_09_12_090852) do
     t.string "username"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
@@ -49,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_09_12_090852) do
     t.index ["category_id"], name: "index_records_on_category_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "accounts"
   add_foreign_key "records", "accounts"
   add_foreign_key "records", "categories"
