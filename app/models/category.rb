bae537b958dfffb78e143bf94e694b278e47ce1f
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-16 12:57:01 +0900

    rubocop導入

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   has_many :record
   belongs_to :account
 
-  validates :name, presence: { message: "は必須項目です。" }
+  validates :name, presence: { message: 'は必須項目です。' }
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,19 +11,19 @@ module Yurukotsu
     config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.time_zone = "Tokyo"
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
     config.to_prepare do
-      Devise::SessionsController.layout "devise"
-      # Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
-      Devise::RegistrationsController.layout "devise_registrations"
-      Devise::ConfirmationsController.layout "devise"
-      Devise::UnlocksController.layout "devise"
-      Devise::PasswordsController.layout "devise_password"
+      Devise::SessionsController.layout 'devise'
+      # Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? 'application' : 'devise' }
+      Devise::RegistrationsController.layout 'devise_registrations'
+      Devise::ConfirmationsController.layout 'devise'
+      Devise::UnlocksController.layout 'devise'
+      Devise::PasswordsController.layout 'devise_password'
     end
   end
 end
