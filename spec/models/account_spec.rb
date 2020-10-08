1af62a90fd10f424b7f45d0f382af23ae411db6c
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-10-08 20:08:35 +0900

    accountモデルテスト追加

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'メールアドレスとパスワードがあれば有効であること' do
+    @account = Account.new(
+      email: 'sample@sample.com',
+      password: 'sample123'
+    )
+    expect(@account).to be_valid
+  end
+
+  it 'メールアドレスがなければ無効であること' do
+    @account = Account.new(email: nil)
+    expect(@account.valid?).to eq(false)
+  end
+
+  it 'パスワードがなければ無効であること' do
+    @account = Account.new(password: nil)
+    expect(@account.valid?).to eq(false)
+  end
+
+  it 'メールアドレスが重複している場合は無効であること' do
+    Account.create(
+      email:      "test@example.com",
+      password:   "test123"
+    )
+
+    @account = Account.new( 
+      email:      "test@example.com",
+      password:   "test456"
+    ) 
+
+    @account.valid?
+    expect(@account.valid?).to eq(false)
+  end
 end
