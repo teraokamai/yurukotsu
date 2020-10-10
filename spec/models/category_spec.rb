b7b9e29d73b53f92473b6c5d9f322537cd4a6cac
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-10-10 14:38:16 +0900

    categoryテスト追記

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  it 'カテゴリ名、アカウントID、作成日、更新日、デフォルトフラグがあれば有効であること' do
+    @account = Account.new( 
+      email:      "test@example.com",
+      password:   "test123"
+    ) 
+    
+    @category = Category.new(
+      name: 'サンプルカテゴリ',
+      account: @account,
+      is_default: false
+    )
+    expect(@category).to be_valid
+  end
+
+  it 'カテゴリ名がなければ無効であること' do
+    @category = Category.new(name: nil)
+    expect(@category.valid?).to eq(false)
+  end
+end
